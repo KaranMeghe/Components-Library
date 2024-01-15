d03cc0b7084df51889352cae080c6c012206ae06
Karan Meghe <karanmeghe87@gmail.com>
2024-01-16 03:17:33 +0530

    Refactor - props ie  className passed on Button Component will be added as className :

--- library/src/Button.js
+++ library/src/Button.js
@@ -1,5 +1,5 @@
 import PropTypes from "prop-types";
-import classNames from "classnames";
+import className from "classnames";
 import { twMerge } from "tailwind-merge";
 
 const Button = ({
@@ -13,20 +13,24 @@ const Button = ({
   rounded,
   ...rest
 }) => {
-  let classes = classNames("flex items-center m-3 px-3 py-1.5 border", {
-    "border-blue-500 bg-blue-500 text-white": primary,
-    "border-gray-900 bg-gray-900 text-white": secondary,
-    "border-green-500 bg-green-500 text-white": success,
-    "border-yellow-400 bg-yellow-400 text-white": warning,
-    "border-red-500 bg-red-500 text-white": danger,
-    "rounded-full": rounded,
-    "bg-white": outline,
-    "text-blue-500": outline && primary,
-    "text-gray-900": outline && secondary,
-    "text-green-500": outline && success,
-    "text-yellow-400": outline && warning,
-    "text-red-500": outline && danger,
-  });
+  let classes = className(
+    rest.className,
+    "flex items-center m-3 px-3 py-1.5 border",
+    {
+      "border-blue-500 bg-blue-500 text-white": primary,
+      "border-gray-900 bg-gray-900 text-white": secondary,
+      "border-green-500 bg-green-500 text-white": success,
+      "border-yellow-400 bg-yellow-400 text-white": warning,
+      "border-red-500 bg-red-500 text-white": danger,
+      "rounded-full": rounded,
+      "bg-white": outline,
+      "text-blue-500": outline && primary,
+      "text-gray-900": outline && secondary,
+      "text-green-500": outline && success,
+      "text-yellow-400": outline && warning,
+      "text-red-500": outline && danger,
+    }
+  );
 
   classes = twMerge(classes);
 
@@ -38,11 +42,14 @@ const Button = ({
 };
 
 Button.propTypes = {
+  // Validating prop types
   primary: PropTypes.bool,
   secondary: PropTypes.bool,
   success: PropTypes.bool,
   warning: PropTypes.bool,
   danger: PropTypes.bool,
+  outline: PropTypes.bool,
+  rounded: PropTypes.bool,
 
   checkVariationValue: ({ primary, secondary, success, warning, danger }) => {
     const count =
