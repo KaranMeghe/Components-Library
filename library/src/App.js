b6e1c666e2d0f328706cde55f3711156924d6334
Karan Meghe <karanmeghe87@gmail.com>
2024-01-16 17:32:13 +0530

    Refactoring and Organization - creating Components and Page folder

--- library/src/App.js
+++ library/src/App.js
@@ -1,53 +1,7 @@
-import Button from "./Button";
+import ButtonPage from "./Pages/ButtonPage";
 
 const App = () => {
-  const handleClick = () => {
-    console.log("Collecting Rest");
-  };
-  const handleMouseOverIn = () => {
-    console.log("in");
-  };
-  const handleMouseOverOut = () => {
-    console.log("out");
-  };
-  return (
-    <div>
-      <div>
-        <Button primary onClick={handleClick}>
-          Primary
-        </Button>
-      </div>
-
-      <div>
-        <Button
-          secondary
-          rounded
-          onMouseOver={handleMouseOverIn}
-          onMouseLeave={handleMouseOverOut}
-        >
-          Secondary
-        </Button>
-      </div>
-
-      <div>
-        <Button success outline>
-          Success
-        </Button>
-      </div>
-
-      <div>
-        <Button warning outline>
-          Warning
-        </Button>
-      </div>
-
-      <div>
-        <Button danger outline rounded>
-          Danger
-        </Button>
-      </div>
-    </div>
-  );
+  return <ButtonPage />;
 };
 
 export default App;
---
+++ library/src/Pages/ButtonPage.js
@@ -0,0 +1,53 @@
+import Button from "../Components/Button";
+
+const ButtonPage = () => {
+  const handleClick = () => {
+    console.log("Collecting Rest");
+  };
+  const handleMouseOverIn = () => {
+    console.log("in");
+  };
+  const handleMouseOverOut = () => {
+    console.log("out");
+  };
+  return (
+    <div>
+      <div>
+        <Button primary onClick={handleClick}>
+          Primary
+        </Button>
+      </div>
+
+      <div>
+        <Button
+          secondary
+          rounded
+          onMouseOver={handleMouseOverIn}
+          onMouseLeave={handleMouseOverOut}
+        >
+          Secondary
+        </Button>
+      </div>
+
+      <div>
+        <Button success outline>
+          Success
+        </Button>
+      </div>
+
+      <div>
+        <Button warning outline>
+          Warning
+        </Button>
+      </div>
+
+      <div>
+        <Button danger outline rounded>
+          Danger
+        </Button>
+      </div>
+    </div>
+  );
+};
+
+export default ButtonPage;
