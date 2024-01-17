f8cee024199a997455159e8a1c5583fcbddf3a54
Karan Meghe <karanmeghe87@gmail.com>
2024-01-18 01:49:58 +0530

    Style - icon and style is added for Accordion component

--- library/src/Components/Accordion.js
+++ library/src/Components/Accordion.js
@@ -1,4 +1,5 @@
 import { useState } from "react";
+import { FaChevronCircleDown, FaChevronCircleUp } from "react-icons/fa";
 
 const Accordion = ({ items }) => {
   const [expandedIndex, setExpandedIndex] = useState(0);
@@ -10,14 +11,28 @@ const Accordion = ({ items }) => {
   const renderdItems = items.map((item, index) => {
     const isExpanded = index === expandedIndex;
 
+    const icon = (
+      <span>
+        {isExpanded ? <FaChevronCircleUp /> : <FaChevronCircleDown />}
+      </span>
+    );
+
     return (
-      <div key={item.id}>
-        <div onClick={() => handleClick(index)}>{item.label}</div>
-        {isExpanded && <div>{item.content}</div>}
+      <div key={item.id} className="w-96">
+        <div
+          onClick={() => handleClick(index)}
+          className="flex items-center justify-between p-3 bg-gray-50 border-b border-1 cursor-pointer"
+        >
+          <h2>{item.label}</h2>
+          <div>{icon}</div>
+        </div>
+
+        {isExpanded && <div className="border-b p-5">{item.content}</div>}
       </div>
     );
   });
-  return <div> {renderdItems}</div>;
+
+  return <div className="border-x border-t rounded"> {renderdItems}</div>;
 };
 
 export default Accordion;
