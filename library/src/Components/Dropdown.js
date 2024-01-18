422f15a17a7ad860b24c69a3f79245494eb4b4b2
Karan Meghe <karanmeghe87@gmail.com>
2024-01-19 03:41:51 +0530

    feat - state and events are setted for dropdown menu list to expand and collapse menu list

--- library/src/Components/Dropdown.js
+++ library/src/Components/Dropdown.js
@@ -1,5 +1,27 @@
-const Dropdown = () => {
-  return <div>Dropdown</div>;
+import { useState } from "react";
+import { FaChevronCircleDown } from "react-icons/fa";
+const Dropdown = ({ options }) => {
+  const [isExpand, setIsExpand] = useState(false);
+
+  const renderedItems = options.map((item) => {
+    return <div key={item.value}>{item.label}</div>;
+  });
+
+  const handleClick = () => {
+    setIsExpand(!isExpand);
+  };
+
+  return (
+    <div className="w-32">
+      <div className="flex justify-between items-center" onClick={handleClick}>
+        <div>Select....</div>
+        <div>
+          <FaChevronCircleDown />
+        </div>
+      </div>
+      {isExpand && <div>{renderedItems}</div>}
+    </div>
+  );
 };
 
 export default Dropdown;
