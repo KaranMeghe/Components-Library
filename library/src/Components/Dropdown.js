2e0d127aaebface1d9e18ec30ee772ae97b4c625
Karan Meghe <karanmeghe87@gmail.com>
2024-01-19 20:18:52 +0530

    Style - styling Dropdown component

--- library/src/Components/Dropdown.js
+++ library/src/Components/Dropdown.js
@@ -14,21 +14,32 @@ const Dropdown = ({ options, value, onChange }) => {
 
   const renderedItems = options.map((item) => {
     return (
-      <div key={item.value} onClick={() => handleOptionClick(item)}>
+      <div
+        key={item.value}
+        className="hover:bg-sky-100 rounded p-1"
+        onClick={() => handleOptionClick(item)}
+      >
         {item.label}
       </div>
     );
   });
 
   return (
-    <div className="w-32 cursor-pointer">
-      <div className="flex justify-between items-center" onClick={handleClick}>
+    <div className="w-32 relative cursor-pointer m-2">
+      <div
+        className="flex justify-between items-center border"
+        onClick={handleClick}
+      >
         <div>{value?.label || "Select...."}</div>
         <div>
           <FaChevronCircleDown />
         </div>
       </div>
-      {isExpand && <div>{renderedItems}</div>}
+      {isExpand && (
+        <div className="absolute top-full border rounded p-3 shadow bg-white w-full">
+          {renderedItems}
+        </div>
+      )}
     </div>
   );
 };
